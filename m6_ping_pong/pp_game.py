7846daa2de2e86da30bfcaa5debcf159ab1b3e97
Dmitriy <dmitriy250300@mail.ru>
2023-04-09 10:53:36 +0300

    08.04.2023

--- m6_ping_pong/pp_game.py
+++ m6_ping_pong/pp_game.py
@@ -36,12 +36,13 @@ class Player(GameSprite):
 win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
-window.fill((200, 255, 255))
+back = (200, 255, 255)
+window.fill(back)
 
 '''спрайты игры'''
 racket1 = Player('racket.png', 30, 200, 4, 50, 150)
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
-ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
 '''шрифты'''
 font.init()
@@ -50,3 +51,48 @@ lose1 = font.render('ИГРОК 1 ПРОИГРАЛ', True, (165, 4, 3))
 lose2 = font.render('ИГРОК 2 ПРОИГРАЛ', True, (165, 4, 3))
 
 '''игровой цикл'''
+speed_x = 3
+speed_y = 3
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if not finish:
+        window.fill(back)
+        racket1.move_left()
+        racket2.move_right()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        #если мяч достигает границы экрана, меняем направление его движения
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        #если мяч улетел дальше ракетки, проигрыш одного из игроков
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
+
+
