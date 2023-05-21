0e3163453b6e20585eb10b4b291da2e1d0d58005
dmitriy <dmitriy250300@mail.ru>
2023-05-21 11:23:52 +0300

    21.05.2023

--- m2_mobile_development/test_ruffier_lesson2/main_app.py
+++ m2_mobile_development/test_ruffier_lesson2/main_app.py
@@ -1,1 +1,82 @@
 # напиши здесь свое приложение
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.scrollview import ScrollView
+
+from instructions import *
+from ruffier import test
+
+
+age = 7
+name = ''
+p1, p2, p3 = 0, 0, 0
+
+
+class InstrScr(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        instr = Label(text=txt_instruction)
+        lbl1 = Label(text='Введите имя:')
+        lbl2 = Label(text='Введите возраст:')
+        self.in_name = TextInput(multiline=False)
+        self.in_age = TextInput(multiline=False)
+        self.btn = Button(text='Начать', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+        self.btn.on_press = self.next
+        line1 = BoxLayout(size_hint=(0.8, None), height='30sp')
+        line2 = BoxLayout(size_hint=(0.8, None), height='30sp')
+        line1.add_widget(lbl1)
+        line1.add_widget(self.in_name)
+        line2.add_widget(lbl2)
+        line2.add_widget(self.in_age)
+        outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
+        outer.add_widget(instr)
+        outer.add_widget(line1)
+        outer.add_widget(line2)
+        outer.add_widget(self.btn)
+        self.add_widget(outer)
+
+    def next(self):
+        global name
+        name = self.in_name.text
+        self.manager.current = 'pulse1'
+
+
+class PulseScr(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        instr = Label(text=txt_test1)
+        lbl_result = Label(text='Введите результат:')
+        self.in_result = TextInput(text='0', multiline=False)
+        self.btn = Button(text='Продолжить', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+        self.btn.on_press = self.next
+        line = BoxLayout(size_hint=(0.8, None), height='30sp')
+        line.add_widget(lbl_result)
+        line.add_widget(self.in_result)
+        outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
+        outer.add_widget(instr)
+        outer.add_widget(line)
+        outer.add_widget(self.btn)
+        self.add_widget(outer)
+    def next(self):
+        global p1
+        p1 = int(self.in_result.text)
+        self.manager.current = 'sits'
+
+
+class HeartCheck(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(InstrScr(name='instr'))
+        sm.add_widget(PulseScr(name='pulse1'))
+
+        return sm
+
+
+app = HeartCheck()
+app.run()
+
+
