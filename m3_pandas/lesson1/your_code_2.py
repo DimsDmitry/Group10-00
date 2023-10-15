6c8b6ca3f77521fe107bd9e9215cbea48c90e494
Dmitriy <dmitriy250300@mail.ru>
2023-10-15 17:22:14 +0300

    15.10.2023

--- m3_pandas/lesson1/your_code_2.py
+++ m3_pandas/lesson1/your_code_2.py
@@ -4,19 +4,31 @@ df = pd.read_csv('GoogleApps.csv')
 # Сколько стоит (Price) самое дешёвое платное приложение (Type == 'Paid)?
 print(df[df['Type'] == 'Paid']['Price'].min())
 
+print(100*'#')
 # Чему равно медианное (median) количество установок (Installs)
 # приложений из категории (Category) "ART_AND_DESIGN"?
-
-
+print(df[df['Category'] == 'ART_AND_DESIGN']['Installs'].median())
+print(100*'*')
 # На сколько максимальное количество отзывов (Reviews) для бесплатных приложений (Type == 'Free')
 # больше максимального количества отзывов для платных приложений (Type == 'Paid')?
-
+free = df[df['Type'] == 'Free']['Reviews'].max()
+paid = df[df['Type'] == 'Paid']['Reviews'].max()
+print('Количество отзывов у бесплатных приложений:', free)
+print('Количество отзывов у платных приложений:', paid)
+print('Разница:', free-paid, '\n')
 
 # Каков минимальный размер (Size) приложения для тинейджеров (Content Rating == 'Teen')?
+result = df[df['Content Rating'] == 'Teen']['Size'].min()
+print(round(result, 3))
 
 
 # *К какой категории (Category) относится приложение с самым большим количеством отзывов (Reviews)?
-
-
+result = df[df['Reviews'] == df['Reviews'].max()]['Category']
+print(result)
+print(100*'@')
 # *Каков средний (mean) рейтинг (Rating) приложений стоимостью (Price) более 20 долларов и 
 # с количеством установок (Installs) более 10000?
+price = df['Price'] > 20
+installs = df['Installs'] > 10000
+result = df[price & installs]['Rating'].mean()
+print(result)
--- m3_pandas/lesson2/test.py
+++ m3_pandas/lesson2/test.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import numpy as np
 
+df = pd.read_csv('GoogleApps.csv')
 
-df = pd.read_csv('GoogleApps.csv', sep=',', nrows=10)
-print(df.groupby(by='Content Rating')['Size'].mean())
 
-print('*' * 100)
 
-print(df.groupby(by='Content Rating')['Size'].agg(['min', 'max']))
+
+# df = pd.read_csv('GoogleApps.csv', sep=',', nrows=10)
+# print(df.groupby(by='Content Rating')['Size'].mean())
+#
+# print('*' * 100)
+#
+# print(df.groupby(by='Content Rating')['Size'].agg(['min', 'max']))
