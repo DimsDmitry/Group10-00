d1f4a84ca9b4307e14acc3752dd7e41440ec32d5
Dmitriy <dmitriy250300@mail.ru>
2024-04-08 21:34:06 +0300

    08.04.2023

---
+++ m6_web_dev/lesson3/task1/sql_queries.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+
+conn = sqlite3.connect('Artistc.db')
+cursor = conn.cursor()
+
+# Вопрос 1. Информация о скольких художниках представлена в базе данных? 
+cursor.execute('SELECT * FROM artists')
+data = cursor.fetchall()
+print('Количество художников в базе:', len(data))
+# Вопрос 2. Сколько женщин (Female) в базе?
+cursor.execute('SELECT * FROM artists WHERE "gender" == "Female"')
+data = cursor.fetchall()
+print('Количество женщин в базе:', len(data))
+
+# Вопрос 3. Сколько человек в базе данных родились до 1900 года?
+cursor.execute('SELECT * FROM artists WHERE "Birth Year" < 1900')
+data = cursor.fetchall()
+print('Родились до 1900 года:', len(data))
+
+# Вопрос 4*. Как зовут самого пожилого художника?
+cursor.execute('SELECT * FROM artists WHERE "Birth Year" < 1900 order by "Birth Year"')
+data = cursor.fetchall()
+print('Самый пожилой художник:', data[0][1])
